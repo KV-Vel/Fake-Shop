781e2162fe502378abd704068d06a5c487d159f4
SprngVel <kraevvalentin98@gmail.com>
2026-01-05 12:11:21 +1000

    feat: added shop page with simple implementation

--- src/pages/ShopPage/ShopPage.test.tsx
+++ src/pages/ShopPage/ShopPage.test.tsx
@@ -1,3 +1,65 @@
+import { createRoutesStub, data, type LoaderFunction } from "react-router";
 import { test } from "vitest";
+import ShopPage from "./ShopPage";
+import { render, screen } from "@testing-library/react";
 
-test.skip("skip this one", () => {});
+const mockData = {
+    products: {
+        data: [
+            {
+                id: "0",
+                name: "test",
+                category: "furniture",
+                description: "good looking furniture",
+                wood_type: "test",
+                finish: "TEST",
+                dimensions: {
+                    depth: 256,
+                    width: 344,
+                    height: 190,
+                },
+                price: 355,
+                weight: 25,
+                image_path: "test_path",
+                stock: 9,
+                sku: "s0_0",
+                status: "active",
+                created_at: "19",
+                updated_at: "10",
+                featured: false,
+                discount_price: 300,
+                tags: null,
+            },
+        ],
+    },
+};
+
+function setupStub(fn: LoaderFunction) {
+    const Stub = createRoutesStub([
+        {
+            path: "/shop",
+            Component: ShopPage,
+            loader: fn,
+        },
+    ]);
+
+    render(<Stub initialEntries={["/shop"]} />);
+}
+
+describe("Shop page tests", () => {
+    test("if api returns data it should be displayed on the screen", async () => {
+        setupStub(() => mockData);
+        const articles = await screen.findAllByRole("article");
+
+        expect(articles).toHaveLength(1);
+    });
+
+    test("If api will return empty array, app will have corresponding message about it", async () => {
+        setupStub(() => ({
+            products: { data: [] },
+        }));
+        const articles = screen.queryAllByRole("article");
+
+        expect(articles).toHaveLength(0);
+    });
+});
--- src/pages/ShopPage/ShopPage.tsx
+++ src/pages/ShopPage/ShopPage.tsx
@@ -1,5 +1,36 @@
+import { Await, useLoaderData, useNavigation } from "react-router";
+import ProductCard from "../../components/ProductCard/ProductCard";
 import styles from "./ShopPage.module.css";
+import { Suspense, type ComponentProps } from "react";
+import React from "react";
+import type { Product, Response } from "../../types/data";
 
 export default function ShopPage() {
-    return <>SHOP</>;
+    const { products } = useLoaderData();
+    console.log(products);
+    if (!products) {
+        return <div>No available data for now. Please come back later :)</div>;
+    }
+
+    return (
+        <section className={styles.shopPageWrapper}>
+            <h2>Explore latest collection</h2>
+            <Suspense fallback={<div>Loading</div>}>
+                <Await resolve={products}>
+                    {({ data }) => (
+                        <div className={styles.productsList}>
+                            {data.map((product) => (
+                                <ProductCard
+                                    key={product.id}
+                                    title={product.name}
+                                    price={product.price}
+                                    imageSource={product.image_path}
+                                />
+                            ))}
+                        </div>
+                    )}
+                </Await>
+            </Suspense>
+        </section>
+    );
 }
