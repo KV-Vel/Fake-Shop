be4cba78fd5f4a90ca620e639da414df74e543eb
SprngVel <kraevvalentin98@gmail.com>
2026-01-05 12:12:48 +1000

    feat: added react router loader to shop page

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -4,12 +4,14 @@ import type { RouteObject } from "react-router";
 import ShopPage from "../pages/ShopPage/ShopPage";
 import CartPage from "../pages/CartPage/CartPage";
 import HomePage from "../pages/HomePage/HomePage";
+import { Suspense } from "react";
 
 const routes: RouteObject[] = [
     {
         path: "/",
         element: <App />,
         errorElement: <ErrorPage />,
+
         children: [
             {
                 index: true,
@@ -18,6 +20,15 @@ const routes: RouteObject[] = [
             {
                 path: "shop",
                 element: <ShopPage />,
+
+                loader: async () => {
+                    const products = fetch(
+                        "https://furniture-api.fly.dev/v1/products?category=sofa",
+                    ).then((resp) => resp.json());
+
+                    return { products };
+                },
+                errorElement: <div>Error during fetch</div>,
             },
             {
                 path: "cart",
